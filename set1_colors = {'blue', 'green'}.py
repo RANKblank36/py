042749692e955c111d182382594001e727de14d2
RANKblank36 <tendosana@gmail.com>
2025-06-26 18:06:20 +0200

    Add files via upload

---
+++ set1_colors = {'blue', 'green'}.py
@@ -0,0 +1,11 @@
+set1_colors = {'blue', 'green'}
+set2_colors = {'blue', 'yellow'}
+
+sym_diff_colors = set1_colors.symmetric_difference(set2_colors)
+print("Symmetric Difference (Colors):", sym_diff_colors)
+
+set1_nums = {1, 2, 3, 4, 5}
+set2_nums = {1, 5, 6, 7, 8, 9}
+
+sym_diff_nums = set1_nums.symmetric_difference(set2_nums)
+print("Symmetric Difference (Numbers):", sym_diff_nums)
